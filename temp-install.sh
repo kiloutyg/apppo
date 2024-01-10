d7459a5e69f7f9abb822b466fba4ddb74169677d
SFDKHISSI <floriandkhissi@gmail.com>
2024-01-10 18:04:25 +0100

    small change to install script

--- temp-install.sh
+++ temp-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/kiloutyg/docauposte2/main/install-docauposte2.sh -O /tmp/install-docauposte2.sh && chmod +x /tmp/install-docauposte2.sh && bash /tmp/install-docauposte2.sh;
+wget https://raw.githubusercontent.com/kiloutyg/docauposte2/main/install-docauposte2.sh   && chmod +x install-docauposte2.sh && bash install-docauposte2.sh;
 
-wget https://raw.githubusercontent.com/kiloutyg/docauposte2/main/install-eFNC2.sh -O /tmp/install-eFNC2.sh && chmod +x /tmp/install-eFNC2.sh && bash /tmp/install-eFNC2.sh;
+wget https://raw.githubusercontent.com/kiloutyg/docauposte2/main/install-eFNC2.sh  && chmod +x install-eFNC2.sh && bash install-eFNC2.sh;
 
 git clone https://github.com/kiloutyg/apppo.git;
 cd apppo;
