b70392ce83324e51039f4b2e543aa3de333316b0
SFDKHISSI <floriandkhissi@gmail.com>
2024-03-21 16:33:16 +0100

    probably no change e

--- temp-install.sh
+++ temp-install.sh
@@ -4,11 +4,7 @@ wget https://raw.githubusercontent.com/kiloutyg/docauposte2/main/install-docaupo
 
 wget https://raw.githubusercontent.com/kiloutyg/efnc/main/install-eFNC2.sh  && chmod +x install-eFNC2.sh && bash install-eFNC2.sh;
 
-<<<<<<< HEAD
 wget https://raw.githubusercontent.com/kiloutyg/posign/main/install-posign.sh  && chmod +x install-posign.sh && bash install-posign.sh;
-=======
-wget https://raw.githubusercontent.com/kiloutyg/posign/main/install-posign.sh   && chmod +x install-posign.sh && bash install-posign.sh;
->>>>>>> 50129cb8ca8989bff3233651c406ee8df613d1ca
 
 git clone https://github.com/kiloutyg/apppo.git;
 cd apppo;
