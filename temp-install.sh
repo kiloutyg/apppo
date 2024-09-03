1e16c6c3ec2692fc57a5b342dba93062917a57e2
SDKHISSI <admin.sdkhissi@SLANLP0041.ad.ponet>
2024-09-03 15:16:50 +0200

    should more less updated stuff

--- temp-install.sh
+++ temp-install.sh
@@ -4,7 +4,22 @@ wget https://raw.githubusercontent.com/kiloutyg/docauposte2/main/install-docaupo
 
 wget https://raw.githubusercontent.com/kiloutyg/efnc/main/install-eFNC2.sh  && chmod +x install-eFNC2.sh && bash install-eFNC2.sh;
 
-wget https://raw.githubusercontent.com/kiloutyg/posign/main/install-posign.sh  && chmod +x install-posign.sh && bash install-posign.sh;
+while true; do
+read -p "Do you want to install posign ? (yes/no)" 
+# Check if the user answered yes or no
+    if [ "${ANSWER}" == "yes" ] || [ "${ANSWER}" == "no" ]; then 
+        break
+        else
+            echo "Please answer by yes or no";
+    fi
+done
+
+if [ "${ANSWER}" == "yes" ]; then
+    wget https://raw.githubusercontent.com/kiloutyg/posign/main/install-posign.sh  && chmod +x install-posign.sh && bash install-posign.sh;
+    else
+    echo "posign not installed";
+fi
+
 
 git clone https://github.com/kiloutyg/apppo.git;
 cd apppo;
