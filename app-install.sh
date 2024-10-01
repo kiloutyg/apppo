58db9120aed4439302b8ec194fc29d19a37e8a0a
SDKHISSI <admin.sdkhissi@SANCLP0031.ad.ponet>
2024-10-01 18:03:07 +0200

    Just added some precision to install script

--- app-install.sh
+++ app-install.sh
@@ -21,7 +21,7 @@ done
 
 # Ask the user if he wants to install the app with podman
 while true; do
-read -p "Do you want to install the app with podman ? (yes/no) : " PODMAN;
+read -p "Do you want to install the apps with podman, otherwise docker will be used ? (yes/no) : " PODMAN;
 # Check if the user answered yes or no
     if [ "${PODMAN}" == "yes" ] || [ "${PODMAN}" == "no" ]; then 
         break
