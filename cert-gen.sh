d38a657310878008be197712f54227ca20bbf605
SDKHISSI <admin.sdkhissi@SANCLP0031.ad.ponet>
2025-02-18 14:05:25 +0100

    Added a script to update the certificates from a cifs volume mount, adapted the cert gen, dynamic and template k8s script

--- cert-gen.sh
+++ cert-gen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+mkdir -p certs/
 openssl req -x509 -nodes -newkey rsa:4096 \
   -keyout certs/lan-apps.key -out certs/lan-apps.crt -days 825 \
-  -subj "/C=FR/ST=Auvergne-Rhone-Alpes/L=Lyon/O=COMPAGNIE PLASTIC OMNIUM SE/CN=lan-apps" \
-  -addext "subjectAltName=DNS:sanclp0031,DNS:sanclp0031.corp.ponet,DNS:sanclp0031.ad.ponet"
+  -subj "/C=FR/ST=Auvergne-Rhone-Alpes/L=Lyon/O=COMPAGNIE PLASTIC OMNIUM SE/CN=lan-apps.corp.ponet" \
+  -addext "subjectAltName=DNS:lan-apps,DNS:${HOSTNAME},DNS:${HOSTNAME}.ad.ponet"
