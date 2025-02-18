5554da2d72b5909e3e6f473829d8cf8255179fc3
SDKHISSI <admin.sdkhissi@SANCLP0031.ad.ponet>
2025-02-18 14:49:22 +0100

    Corrected most issues from sonarcloud analyse

--- cert-gen.sh
+++ cert-gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p certs/
 openssl req -x509 -nodes -newkey rsa:4096 \
-  -keyout certs/lan-apps.key -out certs/lan-apps.crt -days 825 \
+  -keyout certs/lan-apps.key -out certs/lan-apps.corp.ponet.pem -days 825 \
   -subj "/C=FR/ST=Auvergne-Rhone-Alpes/L=Lyon/O=COMPAGNIE PLASTIC OMNIUM SE/CN=lan-apps.corp.ponet" \
   -addext "subjectAltName=DNS:lan-apps,DNS:${HOSTNAME},DNS:${HOSTNAME}.ad.ponet"
